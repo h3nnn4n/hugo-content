ed523277deea446aa57b750d30e5fe6fdcd3aea0
h3nnn4n <uber.renan@gmail.com>
2020-05-12 21:53:08 -0300

    rebuilding site Tue 12 May 2020 09:53:07 PM -03

---
+++ deploy-shootaroo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying to GitHub...\033[0m"
+
+cd public
+git checkout master
+cd ../
+
+hugo
+
+cd public
+
+echo -e "\033[0;32mUpdading shootaroo-dist...\033[0m"
+cd shootaroo-dist
+rm -rf *
+cp -rv ~/progs/Shootaroo/app/* .
+git add .
+git commit -m'version bump'
+git push
+cd ..
+
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+git commit -m "$msg"
+git push origin master
+
+cd ..
+
+git add .
+
+git commit -m "$msg"
+git push origin master
