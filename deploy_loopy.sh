960c59f0dccadd9ae4f379e4745025dfdf13b367
h3nnn4n <uber.renan@gmail.com>
2020-10-26 10:25:43 -0300

    rebuilding site Mon Oct 26 10:25:35 -03 2020 with loopy-dist

--- deploy_loopy.sh
+++ deploy_loopy.sh
@@ -6,9 +6,14 @@ cd public
 git checkout master
 
 echo -e "\033[0;32mUpdading loopy-dist...\033[0m"
+echo -e "\033[0;32mRemoving old loopy-dist...\033[0m"
 rm -rfv loopy-dist
+echo -e "\033[0;32mMaking loopy-dist folder...\033[0m"
 mkdir -vp loopy-dist
-cp -r ~/progs/games/loopy/builds/webgl/* loopy-dist/
+cd loopy-dist
+echo -e "\033[0;32mCopying new files...\033[0m"
+cp -rv ~/progs/games/loopy/builds/webgl/* .
+cd ../
 echo -e "\033[0;32mFinished loopy-dist...\033[0m"
 
 git add .
